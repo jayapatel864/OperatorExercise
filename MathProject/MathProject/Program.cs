9cc4da9ed177cf6f902b443ff9a7d4daeacd5655
unknown <jayapatel864@gmail.com>
2020-06-19 11:51:09 -0500

    finished

---
+++ MathProject/MathProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MathProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var name = Console.ReadLine();
+            var result = Add(2, 4);
+
+            Console.WriteLine(result);
+
+            var multResult = Multiply(5, 10);
+
+            Console.WriteLine(multResult);
+
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+          
+            return num1 * num2;
+
+        }
+    }
+}
+
+
+
