32d8b6dbf8b7e352f62903fc69db44661eed6a49
unknown <jayapatel864@gmail.com>
2020-06-19 11:01:11 -0500

    finished NameProject

---
+++ NameProject/NameProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace NameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var myName = "Jaya Patel";
+            Console.WriteLine("Name:");
+            Console.WriteLine(myName);
+            var myColor = "Green";
+            Console.WriteLine("Favorite Color:");
+            Console.WriteLine(myColor);
+            
+            var myAnimal = "Snail";
+            Console.WriteLine("Favorite Animal:");
+            Console.WriteLine(myAnimal);
+
+            var myBand = "Machine Head";
+            Console.WriteLine("favorite Band:");
+            Console.WriteLine(myBand);
+
+
+
+
+        }
+    }
+}
