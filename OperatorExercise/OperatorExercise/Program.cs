594eaf17e347bb1a3beb840d2efa1de064e960e6
unknown <jayapatel864@gmail.com>
2020-06-22 11:16:16 -0500

    completed

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        public static int Division;
+
+        static void Main(string[] args)
+        {
+          
+            var a = 17;
+            var b = 4;
+            var quotient = (a / b);
+            var remainder = (a % b);
+            
+
+            Console.WriteLine(remainder);
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
